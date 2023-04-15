e49b89cb96dfab243ffbe0448c0c856968d84419
Jose Maria <jmcamposdev@gmail.com>
2023-04-15 11:10:18 +0200

    Adding second Proyect

---
+++ Exercises/Serialisation/TarjetaCreditoSerializacion/src/modelo/ModeloListaTarjetasCredito.java
@@ -0,0 +1,75 @@
+package modelo;
+
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+
+public class ModeloListaTarjetasCredito extends AbstractTableModel{
+
+    private ArrayList<TarjetaCredito> listaTarjetas;
+    private final String[] nombresColumnas = {"Nombre", "Número Tarjeta", "Gasto Total"};
+    
+    public ModeloListaTarjetasCredito() {
+        this.listaTarjetas = new ArrayList<>();
+    }
+    
+    public void añadirTarjeta(TarjetaCredito tarjetaCredito) {
+        this.listaTarjetas.add(tarjetaCredito);
+        this.fireTableDataChanged();
+    }
+    
+    public boolean eliminarTarjeta (int index) {
+        boolean tarjetaEliminada = false;
+        
+        if (index >= 0 && index < listaTarjetas.size()) {
+            listaTarjetas.remove(index);
+            tarjetaEliminada = true;
+        }
+        return  tarjetaEliminada;
+    }
+    
+    public TarjetaCredito getTarjetaCredito (int index) {
+        TarjetaCredito tarjetaCredito = null;
+        
+        if (index >= 0 && index < listaTarjetas.size()) {
+            tarjetaCredito = new TarjetaCredito(listaTarjetas.get(index));
+        }
+        return tarjetaCredito;
+    }
+    
+    
+     @Override
+    public String getColumnName(int index){
+        return this.nombresColumnas[index];
+    }
+    
+    @Override
+    public int getRowCount() {
+        return this.listaTarjetas.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return nombresColumnas.length;
+    }
+
+    @Override
+    public Object getValueAt(int row, int column) {
+        TarjetaCredito c = listaTarjetas.get(row);
+        Object value = null;
+        switch(column){
+            case 0:
+                value = c.getTitular();
+                break;
+            case 1: 
+                value = c.getNumeroTarjeta();
+                break;
+            case 2:
+                value = c.gastado();
+                break;
+        }
+        return value;
+    }
+    
+    
+ }
