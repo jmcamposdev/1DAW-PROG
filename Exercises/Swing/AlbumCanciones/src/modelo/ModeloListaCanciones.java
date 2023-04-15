6c06094c80bce7704bb19b981bf8468c93fbb5f6
Jose Maria <jmcamposdev@gmail.com>
2023-04-15 11:05:48 +0200

    Eliminando Imports innecesarios

--- Exercises/Swing/AlbumCanciones/src/modelo/ModeloListaCanciones.java
+++ Exercises/Swing/AlbumCanciones/src/modelo/ModeloListaCanciones.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import javax.swing.table.AbstractTableModel;
 
 /**
- *
- * @author deifont
+ * 
+* @author deifont
  */
 public class ModeloListaCanciones extends AbstractTableModel{
 
