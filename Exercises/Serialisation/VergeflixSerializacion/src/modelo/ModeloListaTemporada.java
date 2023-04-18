467458b8144996c58df3abe3ebb5033bc7c12428
Jose Maria <jmcamposdev@gmail.com>
2023-04-18 14:49:27 +0200

    Adding Valoración to JTable Temporadas

--- Exercises/Serialisation/VergeflixSerializacion/src/modelo/ModeloListaTemporada.java
+++ Exercises/Serialisation/VergeflixSerializacion/src/modelo/ModeloListaTemporada.java
@@ -16,7 +16,7 @@ public class ModeloListaTemporada extends AbstractTableModel{
     private static DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy"); // Definir el formato deseado
 
     private ArrayList<Temporada> listaTemporadas;
-    private final String[] nombresColumnas = {"Temporada", "Fecha Estreno"};
+    private final String[] nombresColumnas = {"Temporada", "Fecha Estreno", "Valoración"};
     
     public ModeloListaTemporada() {
         this.listaTemporadas = new ArrayList<>();
@@ -75,6 +75,7 @@ public class ModeloListaTemporada extends AbstractTableModel{
         switch(column){
             case 0 -> value = "Temporada "+(row+1);
             case 1 -> value = formato.format(c.getFechaEstreno());
+            case 2 -> value = c.calcularPuntuacion();
         }
         return value;
     }
